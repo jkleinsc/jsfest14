08b9fd5d3a25945938456562dfa4db7017712c3a
John Kleinschmidt <jk@cure.org>
2014-02-28 16:59:40 -0500

    Initial commit

---
+++ work/jsfest14/slide_config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Building Offline First',
+    subtitle: 'JSFest 2014',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/curefav.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ]
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'John Kleinschmidt',
+    company: 'Director of Technology Development<br/>CURE International',
+    twitter: '@jkleinsc',
+    www: 'http://cure.org',
+    slides: 'http://jkleinsc.github.io/jsfest14',
+    offlinefirst: 'http://offlinefirst.org'
+  }]
+};
+
